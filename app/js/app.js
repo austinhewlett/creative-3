55f6c4eecfdde687b46a2f683a5f1c70c35212cf
Austin Hewlett <aust.hewlett@gmail.com>
2017-03-01 15:08:16 -0700

    added validators

--- app/js/app.js
+++ app/js/app.js
@@ -14,9 +14,9 @@ angular.module('AddressBook', [])
                     url: "https://www.drupal.org/files/issues/default-avatar.png" // TODO: VALIDATOR ON IMAGE URL
                 }, {
                     name: "Frodo Baggins",
-                    mobile: "", // TODO: VALIDATOR ON MOBILE
+                    mobile: "",
                     email: "frodo@ofthe.shire",
-                    url: "https://www.drupal.org/files/issues/default-avatar.png" // TODO: VALIDATOR ON IMAGE URL
+                    url: "https://www.google.com/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=0ahUKEwjLsbn6obbSAhUS22MKHSs1CAYQjRwIBw&url=http%3A%2F%2Fpopkey.co%2Fm%2Fxq1RX-elijah%2Bwood-lotr-lord%2Bof%2Bthe%2Brings&bvm=bv.148441817,d.cGc&psig=AFQjCNEghak7-3DS3UiX5fVQrK98IC8kfQ&ust=1488490651533952"
                 }
             ]
         };
@@ -28,8 +28,11 @@ angular.module('AddressBook', [])
         function ($scope, contactFactory) {
             $scope.contacts = contactFactory.contacts;
 
-            $scope.addContact = function () {
+            $scope.addContact = function (contactForm) {
                 if ($scope.name === '') {
+                    return;
+                }
+                if (!isValid($scope.name) ||  !isValid($scope.mobile) || !isValid($scope.email) || !isValid($scope.url)) {
                     return;
                 }
                 $scope.contacts.push({
@@ -46,3 +49,7 @@ angular.module('AddressBook', [])
             }
         }
     ]);
+
+function isValid(formElement){
+    return formElement.$valid;
+}
